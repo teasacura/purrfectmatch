e25fd9e8bb54685092aefb8ea79007894924e7bb
teasacura <teasacuraster@gmail.com>
2018-05-30 16:17:28 -0400

    done.

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,8 @@ Rails.application.routes.draw do
   get '/profiles', to: 'application#index',  as: 'profiles'
   get '/account-type', to: 'application#account_type'
   resources :adopters, :cats, except: [:index]
+  #show? 'Profiles/:id/cat'
+  #show? 'Profiles/:id/adopter'
   resources :admins, only: [:new, :create]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
