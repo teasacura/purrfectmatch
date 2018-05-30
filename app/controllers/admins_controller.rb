b16eebd35989a2db855d2447c904e5960ebb68b7
teasacura <teasacuraster@gmail.com>
2018-05-30 16:25:08 -0400

    added admin form

--- app/controllers/admins_controller.rb
+++ app/controllers/admins_controller.rb
@@ -1,4 +1,20 @@
 class AdminsController < ApplicationController
   def new
+    @admin = Admin.new
+  end
+
+  def create
+    @admin = Admin.new(admin_params)
+    if @admin.save
+      redirect_to profiles
+    else
+      render 'new'
+    end
+  end
+
+  private
+
+  def admin_params
+    params.require(:admin).permit(:name, :password, :password_field)
   end
 end
