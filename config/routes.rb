06e02e56406eebe4581ca071f6b2eb4f1a9defe5
teasacura <teasacuraster@gmail.com>
2018-05-30 15:40:31 -0400

    updated controller actions and views"
    exit
    !!
    q

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,6 @@ Rails.application.routes.draw do
   get '/profiles', to: 'application#index',  as: 'profiles'
   get '/account-type', to: 'application#account_type'
   resources :adopters, :cats, except: [:index]
+  resources :admins, only: [:new, :create]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
